f35e2fc8b859f5f6c489064c7100ef95085c1061
soap925 <cheater980@gmail.com>
2014-06-22 05:28:29 +0700

    Sakuya Project VSfinal2 Porting
    Just A Port Of My Mod.

---
+++ patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "Sakuya!",
+	"servers": [
+		"https://raw.githubusercontent.com/soap925/BurntToast-Mod-Stockhouse/master/Sakuya!/"
+	],
+	"title": "Adds Sakuya As A Playable Character!"
+}
