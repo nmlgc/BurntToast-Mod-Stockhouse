e9132def96c61617281ff6bdf6c97b920f79b06d
nmlgc <nmlgc@nmlgc.net>
2014-06-24 12:21:45 +0200

    Root the Git repository inside the thcrap repository root

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "cheater980@gmail.com",
+	"id": "Toaster",
+	"patches": {
+		"Sakuya!": "Adds Sakuya As A Playable Character!"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/soap925/BurntToast-Mod-Stockhouse/master/"
+	],
+	"title": "BurntToast12's Mod Stockhouse"
+}
